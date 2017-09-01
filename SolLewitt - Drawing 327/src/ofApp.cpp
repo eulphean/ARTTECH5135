ad3686b070013abb9dc3cb69e581b8c612b2c6c4
Amay Kataria <amkatari@microsoft.com>
2017-09-01 18:02:03 -0500

    Complete Sol Leweitt Drawing 327

--- SolLewitt - Drawing 327/src/ofApp.cpp
+++ SolLewitt - Drawing 327/src/ofApp.cpp
@@ -14,6 +14,9 @@ float horizontalBottomOffset = 350;
 float horizontalTopOffset = 175;
 float verticalOffset = 350;
 
+// Trapezium offsets.
+int verticalLineOffset = 5;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
   // Set background color.
@@ -23,7 +26,6 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-  
   // Set the center variables.
   centerX = ofGetWidth()/2;
   centerY = ofGetHeight()/2;
@@ -83,13 +85,52 @@ void drawTrapezium() {
   ofDrawLine(trapeziumVertice[3], trapeziumVertice[1]);
 }
 
+void drawTrapeziumLines() {
+  // x = bottom left to top left, y = bottom left to top left
+  
+  ofSetColor(ofColor::ghostWhite);
+  
+  ofPoint bottomLeft = trapeziumVertice[0];
+  ofPoint topLeft = trapeziumVertice[2];
+  
+  ofPoint bottomRight = trapeziumVertice[1];
+  ofPoint topRight = trapeziumVertice[3];
+  
+  // Section 1 (left)
+  float slopeLeft = (topLeft.y - bottomLeft.y) / (topLeft.x - bottomLeft.x);
+  for (int k = bottomLeft.x; k < topLeft.x; k = k + verticalLineOffset) {
+    // Calculate new y from incrementing x.
+    float newY = bottomLeft.y + slopeLeft * (k - bottomLeft.x);
+    ofDrawLine(k, bottomLeft.y, k, newY);
+  }
+  
+  // Section 2 (middle)
+  for (int i = topLeft.x; i < topRight.x; i = i + verticalLineOffset) {
+    ofDrawLine(i, bottomLeft.y, i, topLeft.y);
+  }
+
+  
+  // Section 3 (right)
+  float slopeRight = (topRight.y - bottomRight.y) / (topRight.x - bottomRight.x);
+  for (int k = bottomRight.x; k >= topRight.x; k = k - verticalLineOffset) {
+    /// Calculate new y from decrementing x.
+    float newY = bottomRight.y + slopeRight * (k - bottomRight.x);
+    ofDrawLine(k, bottomRight.y, k, newY);
+  }
+  
+}
+
 //--------------------------------------------------------------
 void ofApp::draw(){
   // Draw the trapezium.
   drawTrapezium();
+  
   // Draw lines inside the trapezium.
+  drawTrapeziumLines();
+  
   // Draw the center square.
   drawCenterSquare();
+  
 }
 
 //--------------------------------------------------------------
